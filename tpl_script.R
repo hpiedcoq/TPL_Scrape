80fd35f604e6a17f471270ca0650d933f4868700
@hpiedcoq <herve.piedcoq@gmail.com>
2019-07-25 11:05:30 +0200

    Added the Download part

--- tpl_script.R
+++ tpl_script.R
@@ -1,4 +1,5 @@
 setwd("~/Téléchargements/tpl-scrape/TPL_Scrape")
+maindir <- getwd()
 source('constantes.R')
 Sys.sleep(runif(1, 1.5, 5))
 
@@ -43,4 +44,38 @@ while (purlvalue < pmaxvalue){
   gc()
   purlvalue <- purlvalue +1
 }
-write.csv(datasetAll,file = "DatasetAll.csv")
+write.csv(datasetAll,file = "datasetAll.csv")
+
+#Let's prepare for mass download
+#We create a directory called files
+dir.create('files')
+maindir_files <- paste0(maindir,'/files')
+nbUrls <- nrow(datasetAll)
+p <- 1
+#Create a column year to order the directories
+datasetAll['year'] = lapply(test['Document date'], function(x) str_extract(x, "[0-9]{4}"))
+#We loop the dataset to get the document
+#Before we check if the subdirectory year and the subdirectory language exist. If not we create them.
+
+while (p < nbUrls+1){
+  output_dir <- paste0(maindir_files,'/',datasetAll$year[p])
+  file <-unlist(datasetAll['URL'][[1]][p])
+  filename <- paste0(basename(file),'.pdf')
+  #creation of the 1st level of subdir
+  if (!dir.exists(output_dir)){
+    dir.create(output_dir)
+  }
+  #creation of the second level of subdir
+  output_dir <- paste0(output_dir,'/',unlist(datasetAll['Langue Document'][[1]][p]))
+  if (!dir.exists(output_dir)){
+    dir.create(output_dir)
+  }
+  filenamedir <-paste0(output_dir,'/',filename)
+  download.file(file, destfile = filenamedir, method = "wget")
+  #we pause more or less 1.5s between every DL
+  Sys.sleep(runif(1, 1, 2))
+  p <-p+1
+}
+
+
+
